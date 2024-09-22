8761826161f2bf3cbebbcd27e5e9d6a1770f9a2a
유지우 <treeyu12345@naver.com>
2024-09-23 08:26:00 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.06 ms, Memory: 63.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(x: Int, n: Int): LongArray {
+        var answer = LongArray(n)
+        
+        answer[0] = x.toLong()
+        
+        for (i in 1 until n) {
+            answer[i] = (answer[i - 1] + x).toLong()
+        }
+        
+        return answer
+    }
+}
